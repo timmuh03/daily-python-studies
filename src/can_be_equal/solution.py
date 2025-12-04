829119a1e3d8551d0ade0c06bac3da71bea732cc
Vendrid <timmuh03@gmail.com>
2025-12-04 11:32:22 -0500

    simple Counting then comparing to solve.

--- src/can_be_equal/solution.py
+++ src/can_be_equal/solution.py
@@ -8,9 +8,37 @@ Return true if you can make arr equal to target or false otherwise.
 """
 
 
-
+from collections import Counter
 
 class Solution:
     def canBeEqual(self, target: list[int], arr: list[int]) -> bool:
-        return False
+        return Counter(arr) == Counter(target)
         
+        #THE BELOW CODE IS BECAUSE I WANTED TO REMEMBER HOW TO REVERSE SUBSTRINGS
+        # fun_target = target
+        # fun_arr = arr        
+        # count = 0
+        
+        # print('*' * 10, 'start', '*' * 10)
+    
+        # while fun_target != fun_arr and count < len(arr):
+        #     print('count', count)
+        #     if fun_arr[count] not in fun_target:
+        #         return False
+            
+        #     for i in range(count, len(fun_arr)):
+        #         print(fun_arr, fun_target)
+        #         if fun_arr[i] == fun_target[count]:
+        #             print(fun_arr[i], fun_target[count], "<----Match")
+        #             print(count, i)
+        #             reverse fun_arr[count:i]
+        #             for pointer in range(len(fun_arr[count:i])):
+        #                 print('reversing')
+        #                 fun_arr[count + pointer], fun_arr[i - pointer] = fun_arr[i - pointer], fun_arr[count + pointer]                 
+                    
+        #     count += 1
+        # return arr == target
+        
+    
+sol = Solution()
+print(sol.canBeEqual([1,2,3,4], [2,4,1,3]))
