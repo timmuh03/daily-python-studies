ca0ee95654fbe2041d04bd75582644282f421705
Vendrid <timmuh03@gmail.com>
2025-12-06 09:49:00 -0500

    last tweak wasn't saved yesterday. Here's the final version

--- src/search_instert_position/solution.py
+++ src/search_instert_position/solution.py
@@ -22,10 +22,11 @@ Example 3:
 
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        left, right = 0, len(nums)
+        left, right = 0, len(nums) - 1
         
         while left <= right:
             mid = (left + right) // 2
+            print(mid)
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
@@ -34,3 +35,6 @@ class Solution:
                 right = mid - 1
                 
         return left
+    
+sol = Solution()
+print(sol.searchInsert([1,3,5,6], 7))
