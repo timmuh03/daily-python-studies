1b54a3c2f525a7f89c6ee1dac3c9e3e333482062
Tim Muhlestein <timmuh03@gmail.com>
2025-12-12 09:52:20 -0500

    simple class sestup for manipulating class attributes. learned how to use seperate test functions to make testing easier to understand.

---
+++ problems/parking_system/test_solution.py
@@ -0,0 +1,60 @@
+import pytest
+from .solution import ParkingSystem
+
+
+def test_initialization():
+    ps = ParkingSystem(1, 2, 3)
+    assert ps.big == 1
+    assert ps.medium == 2
+    assert ps.small == 3
+
+
+def test_add_big_car_success():
+    ps = ParkingSystem(1, 0, 0)
+    assert ps.addCar(1) is True
+    # now big should be 0
+    assert ps.big == 0
+
+
+def test_add_big_car_fail_when_full():
+    ps = ParkingSystem(1, 0, 0)
+    assert ps.addCar(1) is True
+    assert ps.addCar(1) is False  # no more big spots
+
+
+def test_add_medium_car():
+    ps = ParkingSystem(0, 2, 0)
+    assert ps.addCar(2) is True
+    assert ps.addCar(2) is True
+    assert ps.addCar(2) is False  # third medium car should fail
+
+
+def test_add_small_car():
+    ps = ParkingSystem(0, 0, 1)
+    assert ps.addCar(3) is True
+    assert ps.addCar(3) is False
+
+
+def test_mixed_usage():
+    ps = ParkingSystem(1, 1, 1)
+
+    # all should work once
+    assert ps.addCar(1) is True
+    assert ps.addCar(2) is True
+    assert ps.addCar(3) is True
+
+    # all should now be full
+    assert ps.addCar(1) is False
+    assert ps.addCar(2) is False
+    assert ps.addCar(3) is False
+
+
+@pytest.mark.parametrize("carType", [1, 2, 3])
+def test_invalid_car_type_does_not_crash(carType):
+    """
+    Depending on how you implement addCar, invalid types may simply do nothing.
+    Test that calling with valid types works but does not crash.
+    """
+    ps = ParkingSystem(1, 1, 1)
+    # Should return boolean; calling with valid types is safe
+    assert isinstance(ps.addCar(carType), bool)
