cf16ee6e24b2db73f88f965cc2c54c92f8caaf57
Tim Muhlestein <timmuh03@gmail.com>
2025-12-15 10:53:30 -0500

    final solution. Learned about using a lambda for key= in min()

--- problems/most_frequent_even/solution.py
+++ problems/most_frequent_even/solution.py
@@ -4,22 +4,15 @@ Given an integer array nums, return the most frequent even element.
 
 If there is a tie, return the smallest one. If there is no such element, return -1.
 """
+from collections import Counter
+
 
 class Solution:
     def mostFrequentEven(self, nums: list[int]) -> int:
-        evens = {}
-        for num in nums:
-            if num % 2 == 0:
-                evens[num] = evens.get(num, 0) + 1
-                
-        max_freq = max(evens.values())
+            # get dict items only if they're divisible by 2
+        evens = Counter(num for num in nums if num % 2 == 0)
         
         if not evens:
             return -1
-        
-        most_freq = []
-        for num, freq in evens.items():
-            if freq == max_freq:
-                most_freq.append(num)
-                
-        return min(most_freq)
+            # -evens[x] puts the most occuring nums as least, ties broken by smallest x
+        return min(evens, key=lambda x: (-evens[x], x))
