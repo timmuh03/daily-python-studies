0b3b1ca92464faa65e86bfdc84dc906fcad48de0
Tim Muhlestein <timmuh03@gmail.com>
2025-12-15 08:24:28 -0500

    initial commit for most_frequent_even with test cases.

---
+++ problems/most_frequent_even/solution.py
@@ -0,0 +1,10 @@
+"""
+Docstring for problems.most_frequent_even.solution
+Given an integer array nums, return the most frequent even element.
+
+If there is a tie, return the smallest one. If there is no such element, return -1.
+"""
+
+class Solution:
+    def mostFrequentEven(self, nums: list[int]) -> int:
+        return -1
---
+++ problems/most_frequent_even/test_solution.py
@@ -0,0 +1,21 @@
+import pytest
+from .solution import Solution
+
+@pytest.mark.parametrize("nums, expected", [
+    ([0, 0, 0, 0],
+     0),
+    ([1, 1, 1, 2],
+    2),
+    ([10000000, 1, 3, 3],
+    10000000),
+    ([-2, -2, 0, 1],
+     -2),
+    ([2, 2, 4, 4, 6],
+     2),
+    ([-2, -2, -4, -4, -6],
+     -4)
+])
+
+def test_mostFrequentEven(nums, expected):
+    result = Solution().mostFrequentEven(nums)
+    assert result == expected, f"Failed for nums:{nums}, expected:{expected}, result:{result}"
