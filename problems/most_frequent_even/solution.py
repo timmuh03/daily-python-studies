87eba74d0b1fd1fb70f201ee625ffdb33c0d58d2
Tim Muhlestein <timmuh03@gmail.com>
2025-12-15 09:31:42 -0500

    initial solution that passes all tests.

--- problems/most_frequent_even/solution.py
+++ problems/most_frequent_even/solution.py
@@ -7,4 +7,19 @@ If there is a tie, return the smallest one. If there is no such element, return 
 
 class Solution:
     def mostFrequentEven(self, nums: list[int]) -> int:
-        return -1
+        evens = {}
+        for num in nums:
+            if num % 2 == 0:
+                evens[num] = evens.get(num, 0) + 1
+                
+        max_freq = max(evens.values())
+        
+        if not evens:
+            return -1
+        
+        most_freq = []
+        for num, freq in evens.items():
+            if freq == max_freq:
+                most_freq.append(num)
+                
+        return min(most_freq)
