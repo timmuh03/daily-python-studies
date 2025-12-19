3f1157a8b0006c5493aedac8115fdd87528af71e
Tim Muhlestein <timmuh03@gmail.com>
2025-12-19 17:06:14 -0500

    inital commit for strong_password_checker_II

---
+++ problems/strong_password_checker_II/test_solution.py
@@ -0,0 +1,18 @@
+import pytest
+from .solution import Solution
+
+
+@pytest.mark.parametrize("password, expected", [
+    ("IloveLe3tcode!",
+     True),
+    
+    ("Me+You--IsMyDream",
+    False),
+    
+    ("1aB",
+     False)
+])
+
+def test_strong_password_checker_II(password, expected):
+    result = Solution().strongPasswordCheckerII(password)
+    assert result == expected, f"Failed on password:{password}, expected:{expected}, result:{result}"
