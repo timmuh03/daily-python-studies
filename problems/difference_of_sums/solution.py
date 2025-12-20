b0e0db2659e120b35833caea82e669b0333b0ce4
Tim Muhlestein <timmuh03@gmail.com>
2025-12-20 10:15:19 -0500

    refactored to run only one loop instead of two

--- problems/difference_of_sums/solution.py
+++ problems/difference_of_sums/solution.py
@@ -11,6 +11,16 @@ Return the integer num1 - num2.
 
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        num1 = sum(num for num in range(1, n + 1) if num % m != 0)
-        num2 = sum(num for num in range(1, n + 1) if num % m == 0)
+        num1 = 0
+        num2 = 0
+        
+        
+        for num in range(1, n + 1):
+            if num % m != 0:
+                num1 += num
+            elif num % m == 0:
+                num2 += num
+            
+        # num1 = sum(num for num in range(1, n + 1) if num % m != 0)
+        # num2 = sum(num for num in range(1, n + 1) if num % m == 0)
         return num1 - num2
