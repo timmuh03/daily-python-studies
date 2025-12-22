0003deb4774a8b7504cee1474fdd56d61a563320
Tim Muhlestein <timmuh03@gmail.com>
2025-12-22 08:49:41 -0500

    initial commit for prefixes_div_by_5

---
+++ problems/prefixes_div_by_5/solution.py
@@ -0,0 +1,14 @@
+"""
+Docstring for problems.prefixes_div_by_5.solution
+You are given a binary array nums (0-indexed).
+
+We define xi as the number whose binary representation is the subarray nums[0..i] 
+(from most-significant-bit to least-significant-bit).
+
+For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
+"""
+
+class Solution:
+    def prefixesDivBy5(self, nums: list[int]) -> list[bool]:
+        return [False]
---
+++ problems/prefixes_div_by_5/test_solution.py
@@ -0,0 +1,14 @@
+import pytest
+from .solution import Solution
+
+
+@pytest.mark.parametrize("nums, expected", [
+    ([0,1,1], [True, False, False]),
+    
+    ([1,1,1], [False, False, False])
+])
+
+
+def test_prefixes_div_by_5(nums, expected):
+    result = Solution().prefixesDivBy5(nums)
+    assert result == expected, f"Failed on nums:{nums}, expected:{expected}, result:{result}"
