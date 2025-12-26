d151146f233bc68cb4996a0521872f4f8772fa79
Tim Muhlestein <timmuh03@gmail.com>
2025-12-26 15:04:54 -0500

    updated check_inclusion to show I know how to optimize using mismatch tracking if needed.

--- problems/medium/check_inclusion/solution.py
+++ problems/medium/check_inclusion/solution.py
@@ -17,10 +17,11 @@ from collections import Counter
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        # using counter to produce easily readable code
         perm = Counter(s1)
         window = Counter(s2[:len(s1)])
         for i, char in enumerate(s2[:-len(s1)]):
-            print(f"perm:{perm}, window:{window}")
             if window == perm:
                 return True
             window[char] -= 1
@@ -30,3 +31,46 @@ class Solution:
             
             
         return window == perm
+        
+        # *********************************************************
+        # this code does the same but with lower constant overhead
+        # using a fixed-size array and mismatch tracking
+        # *********************************************************
+        #
+        # if len(s1) > len(s2):
+        #     return False
+        
+        # def update_diff(idx, delta):
+        #     nonlocal diff_count
+        #     before = char_bucket[idx]
+        #     char_bucket[idx] += delta
+        #     after = char_bucket[idx]
+        #     if before == 0 and after != 0:
+        #         diff_count += 1 
+        #     elif before !=0 and after == 0:
+        #         diff_count -= 1
+        
+        # char_bucket = [0] * 26
+        # diff_count = 0
+        # window = len(s1)
+
+        # for i in range(len(s1)):
+            
+        #     update_diff(ord(s1[i]) - ord('a'), 1)
+        #     update_diff(ord(s2[i]) - ord('a'), -1)
+                
+        # if diff_count == 0:
+        #     return True
+                
+        # for i in range(0, len(s2) - window):
+        #     outgoing = s2[i]
+        #     incoming = s2[i+window]
+            
+        #     update_diff(ord(outgoing) - ord('a'), 1)
+        #     update_diff(ord(incoming) - ord('a'), -1)
+            
+        #     if diff_count == 0:
+        #         return True
+            
+        # return False
+    
