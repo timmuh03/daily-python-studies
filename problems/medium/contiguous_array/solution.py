00114bc9711a28fc30cebf34b0ac668d130fa0dd
Tim Muhlestein <timmuh03@gmail.com>
2026-01-05 09:38:27 -0500

    solution for contiguous_array using a balance indicator, and tracking first occurance of each balnce value.

--- problems/medium/contiguous_array/solution.py
+++ problems/medium/contiguous_array/solution.py
@@ -12,4 +12,21 @@ Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0
 
 class Solution:
     def findMaxLength(self, nums: list[int]) -> int:
-        return -1
+        balance = best = 0
+        counts = {0: -1}
+
+        for i, num in enumerate(nums):
+            if num == 0:
+                balance -= 1
+            elif num == 1:
+                balance += 1
+
+            if balance in counts:
+                valid = i - counts[balance]
+                if valid > best:
+                    best = valid
+            else:
+                counts[balance] = i
+
+
+        return best
